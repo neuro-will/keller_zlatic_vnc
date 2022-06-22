3ed004b9d48eab0987a902219209e7a48cf7d988
Will <bishopw@janelia.hhmi.org>
2022-06-22 13:22:49 -0400

    Updated map opening script.

--- spont_whole_brain_stats/open_map.py
+++ spont_whole_brain_stats/open_map.py
@@ -2,26 +2,28 @@
 import os
 from pathlib import Path
 
-pre_beh = 'G'
-suc_beh = 'B'
+# Specify location of collection 
+coll_folder = r'/Volumes/bishoplab/projects/keller_vnc/results/single_subject/new_model_maps/all_supervoxels_merged'
 
-map_type = 'coef_p_vls_comb'
-map_ext = '.mp4'
+# Specify the type of behavior we want to look at
+beh_str = 'beh_H'
 
-options = {'clean_event_def': 'decision',
-           'pool_succeeding_turns': 'False',
-           'window_length': '3',
-           'window_offset': '0'}
-
-coll_folder = r'/Volumes/bishoplab/projects/keller_vnc/results/single_subject/spont_window_sweep/spont_window_sweep'
+# Specify the options for the map we want to look for
+options = {'map_type': 'orig_fit', 
+           'window_length': 1, 
+           'window_offset': -1, 
+           'pool_succeeding_turns': True, 
+           'pool_preceeding_turns': True, 
+	   'co_th': 6, 
+	   'q_start_offset': 1}
 
 ## Open the file
-f_name = pre_beh + '_' + suc_beh + '__' + map_type + '_'
+f_name = beh_str + '__coef_p_vls_comb_'
 
 for key, vl in options.items():
-    f_name += key + '_' + vl + '_'
+    f_name += key + '_' + str(vl) + '_'
 
-f_name = f_name[0:-1] + map_ext
+f_name = f_name[0:-1] + '.mp4'
 f_path = Path(coll_folder) / f_name
 
 os.system('open ' + str(f_path))
