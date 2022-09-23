3ad0d93688a5d6e9b8ae0e2a417e4b7d665eb374
Will <bishopw@janelia.hhmi.org>
2022-09-22 22:05:31 -0400

    Small changes.

--- spont_whole_brain_stats/open_map.py
+++ spont_whole_brain_stats/open_map.py
@@ -6,8 +6,8 @@ from pathlib import Path
 #coll_folder = r'/Volumes/bishoplab/projects/keller_vnc/results/single_subject/new_model_maps_v0/all_supervoxels_v0_collection'
 #coll_folder = r'/Volumes/bishoplab/projects/keller_vnc/results/single_subject/new_model_maps_v1/brain_only_v1_collection'
 #coll_folder = r'/Volumes/bishoplab/projects/keller_vnc/results/single_subject/new_model_maps_v1/cell_bodies_v1_collection'
-coll_folder = r'/Volumes/bishoplab/projects/keller_vnc/results/single_subject/new_model_maps_v1/whole_specimen_rois_collection'
-#coll_folder = r'/Volumes/bishoplab/projects/keller_vnc/results/single_subject/new_model_maps_v1/segments_3_13_13_collection'
+#coll_folder = r'/Volumes/bishoplab/projects/keller_vnc/results/single_subject/new_model_maps_v1/whole_specimen_rois_collection'
+coll_folder = r'/Volumes/bishoplab/projects/keller_vnc/results/single_subject/new_model_maps_v1/segments_3_13_13_collection'
 
 # Specify the type of behavior we want to look at
 beh_str = 'beh_F'
@@ -15,7 +15,7 @@ beh_str = 'beh_F'
 # Specify the options for the map we want to look for
 options = {'map_type': 'orig_fit', 
            'window_length': 3,
-           'window_offset': -7,
+           'window_offset': 0,
            'pool_succeeding_turns': True, 
            'pool_preceeding_turns': True, 
 	   'co_th': 3,
